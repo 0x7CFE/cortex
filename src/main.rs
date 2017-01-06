2f00e5ba3dd5c9337605540e7e615a65d47201a7
Dmitry Kashitsyn <korvin@deeptown.org>
2017-01-06 12:35:21 +0700

    Rounds detector freqs to nearest fourier bin

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,13 @@ use clap::{Arg, App};
 mod sound;
 use sound::Detector;
 
+use std::f32::consts::PI;
+
 fn detector_freq(index: usize) -> f32 {
-    15. + 5. * index as f32 + ((index as f32 - 5.) / 16.).exp()
+    let ideal_freq = 15. + 5. * index as f32 + ((index as f32 - 5.) / 16.).exp();
+    let fft_freq = (ideal_freq / sound::BASE_FREQUENCY).trunc() * sound::BASE_FREQUENCY;
+
+    fft_freq
 }
 
 fn main() {
@@ -37,27 +42,23 @@ fn main() {
     let input_filename = options.value_of("input").unwrap();
     let mut detectors = Vec::new();
 
-
     // Populating detectors from 0Hz to ~1KHz with ~2683Hz
     for i in 1 .. 129 {
         let freq = detector_freq(i);
-        let band = 2. * (freq - detector_freq(i-1));
+        let band = 2. * (detector_freq(i+1) - freq);
 
-        detectors.push(Detector::new(freq, band, -5.));
-        detectors.push(Detector::new(freq, band, -15.));
-        detectors.push(Detector::new(freq, band, -25.));
-        detectors.push(Detector::new(freq, band, -35.));
+//         for phase in vec![ -PI, -3.*PI/4., -PI/2., -PI/4., 0., PI/4., PI/2., 3.*PI/4., PI]
+        for phase in vec![ -PI, -PI/2., 0., PI/2., PI ]
+        {
+            detectors.push(Detector::new(freq, band, -5.,  phase));
+            detectors.push(Detector::new(freq, band, -15., phase));
+            detectors.push(Detector::new(freq, band, -25., phase));
+            detectors.push(Detector::new(freq, band, -35., phase));
+            //detectors.push(Detector::new(freq, band, -45., phase));
+        }
 
         println!("detector[{:3}]\tfreq {:.2},\tband {:.2}", i, freq, band);
     }
-
-//     detectors.push(Detector::new(516.7969, 50.0, -12.0));
-//     detectors.push(Detector::new(990.52734, 50.0, -5.0));
-//     detectors.push(Detector::new(1050.52734, 50.0, -5.0));
-
-//    detectors.push(Detector::new(1990.0, 50.0, -15.0));
-//     detectors.push(Detector::new(990.0, 50.0, -25.0));
-//     detectors.push(Detector::new(990.0, 50.0, -35.0));
 
     let mask = sound::analyze_file(input_filename, &detectors);
     println!("{} detectors, mask size {}", detectors.len(), mask.len());
@@ -68,7 +69,7 @@ fn main() {
             print!("{:04} : ", i / detectors.len() + 1);
         }
 
-        if i % (detectors.len() / 4) == 0 {
+        if i % (detectors.len() / 16) == 0 {
             print!("\n        ");
         }
 
--- src/sound.rs
+++ src/sound.rs
@@ -1,7 +1,7 @@
 
 use num_complex::*;
 use num_traits::Float;
-// use std;
+use std::f32::consts::PI;
 
 use dft;
 use dft::{Operation, Plan};
@@ -11,7 +11,8 @@ pub use bit_vec::BitVec;
 use hound;
 
 // TODO Move to the Detector as individual field
-const AMPLITUDE_DISPERSION_DB: f32 = 5.;
+const AMPLITUDE_DEVIATION_DB: f32 = 5.;
+const PHASE_DEVIATION_DB: f32 = PI / 4.;
 
 pub type Cplx = Complex<f32>;
 
@@ -20,26 +21,25 @@ pub type Samples  = Vec<Cplx>;
 pub type Spectrum = Vec<Cplx>;
 pub type SpectrumSlice = [Cplx];
 
-pub const SAMPLE_RATE:     usize = 44100;
+pub const SAMPLE_RATE:     usize = 96000; // 44100;
 pub const NUM_POINTS:      usize = 8192;
 pub const BASE_FREQUENCY:  f32 = (SAMPLE_RATE as f32) / (NUM_POINTS as f32);
 
 #[derive(Copy, Clone, Debug, Default)]
 pub struct Detector {
-    freq: f32, // base detector frequency
-    band: f32, // frequency range
-    amp:  f32, // amplitude
-//     phase: i16,
+    freq:  f32, // base detector frequency
+    band:  f32, // frequency range
+    amp:   f32, // amplitude
+    phase: f32, // phase
 }
 
 impl Detector {
-    pub fn new(freq: f32, band: f32, amp: f32/*, phase: i16*/) -> Detector {
+    pub fn new(freq: f32, band: f32, amp: f32, phase: f32) -> Detector {
         Detector {
             freq: freq,
             band: band,
             amp: amp,
-
-//             phase: phase
+            phase: phase,
         }
     }
 }
@@ -72,6 +72,42 @@ pub fn filter_detectors_inplace(spectrum: &SpectrumSlice, detectors: &[Detector]
     for detector in detectors {
         // Each detector operates only in the fixed part of the spectrum
         // Selecting potentially interesting spectrum slice to check
+
+        let index = (detector.freq / BASE_FREQUENCY).round() as usize;
+
+        println!("detector freq {} ± {}, amp {} ± {} dB, phase {}",
+            detector.freq,
+            detector.band,
+            detector.amp,
+            AMPLITUDE_DEVIATION_DB,
+            detector.phase
+        );
+
+        // Selecting the entry with the largest amplitude
+        let amplitude = (spectrum[index].norm() * 2.0) / NUM_POINTS as f32;
+        let phase = spectrum[index].arg();
+
+        // Treating detector as active if max amplitude lays within detector's selectivity range
+        let amp_match   = (decibel(amplitude).abs() - detector.amp.abs()).abs() < AMPLITUDE_DEVIATION_DB;
+        let phase_match = ((phase + PI) - (detector.phase + PI)).abs() < PHASE_DEVIATION_DB;
+        let is_active   = amp_match && phase_match;
+
+        println!("signal amplitude {} → {} dB, phase {}{}\n",
+            amplitude,
+            decibel(amplitude),
+            phase,
+            if is_active { ", **MATCH**" } else { "" }
+        );
+
+        result.push(is_active);
+    }
+}
+
+pub fn filter_detectors_inplace2(spectrum: &SpectrumSlice, detectors: &[Detector], result: &mut BitVec) {
+    // Iterating through all detectors filtering out activity
+    for detector in detectors {
+        // Each detector operates only in the fixed part of the spectrum
+        // Selecting potentially interesting spectrum slice to check
         let lo = ((detector.freq - detector.band).abs() / BASE_FREQUENCY).round() as usize;
         let hi = ((detector.freq + detector.band).abs() / BASE_FREQUENCY).round() as usize;
 
@@ -82,30 +118,35 @@ pub fn filter_detectors_inplace(spectrum: &SpectrumSlice, detectors: &[Detector]
 
         let range = &spectrum[lo .. hi + 1];
 
-        println!("detector freq {} ± {}, amp {} ± {} dB, selected freq range is {} .. {}",
+        println!("detector freq {} ± {}, amp {} ± {} dB, phase {}, selected freq range is {} .. {}",
             detector.freq,
             detector.band,
             detector.amp,
-            AMPLITUDE_DISPERSION_DB,
+            AMPLITUDE_DEVIATION_DB,
+            detector.phase,
             freq(lo),
             freq(hi)
         );
 
         // Selecting the entry with the largest amplitude
-        let (index, amplitude) = range
+        let (index, amplitude, phase) = range
             .iter()
             .enumerate()
-            .map(|(i, c)| (i, (c.norm() * 2.0) / NUM_POINTS as f32))
-            .max_by(|&(_, x), &(_, y)| float_cmp(x, y, 0.00001))
+            .map(|(i, c)| (i, (c.norm() * 2.0) / NUM_POINTS as f32, c.arg()))
+            //.max_by(|&(_, x, _), &(_, y, _)| x.partial_cmp(&y).unwrap_or(Ordering::Less))
+            .max_by(|&(_, x, _), &(_, y, _)| float_cmp(x, y, 0.00001))
             .unwrap();
 
         // Treating detector as active if max amplitude lays within detector's selectivity range
-        let is_active = (decibel(amplitude).abs() - detector.amp.abs()).abs() < AMPLITUDE_DISPERSION_DB;
+        let amp_match   = (decibel(amplitude).abs() - detector.amp.abs()).abs() < AMPLITUDE_DEVIATION_DB;
+        let phase_match = ((phase + PI) - (detector.phase + PI)).abs() < PHASE_DEVIATION_DB;
+        let is_active   = amp_match && phase_match;
 
-        println!("signal frequency {}, amplitude {} → {} dB {}\n",
+        println!("signal frequency {}, amplitude {} → {} dB, phase {}{}\n",
             freq(lo+index),
             amplitude,
             decibel(amplitude),
+            phase,
             if is_active { ", **MATCH**" } else { "" }
         );
 
@@ -151,7 +192,7 @@ pub fn analyze_file(filename: &str, detectors: &[Detector]) -> BitVec {
 pub fn generate_file(filename: &str, detectors: &[Detector], mask: &BitVec) {
     let wav_header = hound::WavSpec {
         channels: 1,
-        sample_rate: 44100,
+        sample_rate: SAMPLE_RATE as u32, //44100,
         bits_per_sample: 16,
         sample_format: hound::SampleFormat::Int,
     };
@@ -169,7 +210,7 @@ pub fn generate_file(filename: &str, detectors: &[Detector], mask: &BitVec) {
 
         let mut num_taken = 0;
 
-        for (freq, amp) in mask_iter
+        for (frequency, amplitude, phase) in mask_iter
             .clone()
             .take(detectors.len())
             .filter(|_| { num_taken += 1; true })
@@ -178,6 +219,7 @@ pub fn generate_file(filename: &str, detectors: &[Detector], mask: &BitVec) {
             .map(|(index, _)| {
                 let frequency = detectors[index].freq;
                 let amplitude = invert_decibel(detectors[index].amp) / 2.0;// * NUM_POINTS as f32;
+                let phase = detectors[index].phase;
 
                 println!("chunk {}, active detector[{}]: freq {}, amplitude {} dB = {}",
                     chunk,
@@ -186,17 +228,18 @@ pub fn generate_file(filename: &str, detectors: &[Detector], mask: &BitVec) {
                     detectors[index].amp,
                     amplitude);
 
-                // Phase was lost during analysis
-                (frequency, amplitude)
+                (frequency, amplitude, phase)
             })
         {
-            let bin = (freq / BASE_FREQUENCY).round() as usize;
+            let bin = (frequency / BASE_FREQUENCY).round() as usize;
 
-            println!("writing freq {} to bin[{}] = bin[{}] = {}\n", freq, bin, NUM_POINTS-bin, amp);
+            println!("writing freq {} to bin[{}] = bin[{}] = {}\n", frequency, bin, NUM_POINTS-bin, amplitude);
 
-            if spectrum[bin].re < amp {
-                spectrum[bin].re = amp;
-                // spectrum[NUM_POINTS - bin].re = amp;
+            let value = Cplx::from_polar(&amplitude, &phase);
+
+            if spectrum[bin].norm() < value.norm() {
+                spectrum[bin] = value;
+                // spectrum[NUM_POINTS - bin].re = amplitude;
             }
         }
 
@@ -211,12 +254,13 @@ pub fn generate_file(filename: &str, detectors: &[Detector], mask: &BitVec) {
         dft::transform(&mut spectrum, &plan);
 
         let max_sample = spectrum.iter().max_by(|&x, &y| float_cmp(x.re, y.re, 0.00001)).unwrap().re;
+        //let max_sample = spectrum.iter().max_by(|x, y| x.norm().partial_cmp(&y.norm()).unwrap_or(Ordering::Less)).unwrap().re;
 
         for sample in spectrum {
             total_samples += 1;
 
-            let amplitude = (i16::max_value() - 100) as f32;
-            writer.write_sample((sample.re / max_sample * amplitude) as i16).unwrap();
+            let amplitude = (i16::max_value() - 1000) as f32;
+            writer.write_sample(((sample.re / max_sample) * (amplitude / 2.)) as i16).unwrap();
         }
 
         println!("total samples so far {}", total_samples);
