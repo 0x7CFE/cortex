5cf4b4e1bfc9ea325ead50e5edfe338591375b12
Dmitry Kashitsyn <korvin@deeptown.org>
2017-01-10 19:42:53 +0700

    Adds Idea wrapper over BitVec

--- src/memory.rs
+++ src/memory.rs
@@ -8,10 +8,60 @@ use bit_vec::BitVec;
 
 use sound::{Spectrum, Cplx, Detector};
 
-#[derive(Hash, Eq, PartialEq, Debug)]
-struct FragmentKey(BitVec);
 
-const FRAGMENT_SLICES: usize = 8;
+#[derive(Hash, Eq, PartialEq, Debug)]
+struct Idea {
+    data: BitVec,
+    bits_set: usize,
+    leading_zeros: usize,
+    trailing_zeros: usize,
+}
+
+impl Idea {
+    fn new() -> Idea {
+        Idea {
+            data: BitVec::new(),
+            bits_set: 0,
+            leading_zeros: 0,
+            trailing_zeros: 0
+        }
+    }
+
+    fn bits(&self) -> &BitVec {
+        &self.data
+    }
+
+    fn from_bitvec(bits: BitVec) -> Idea {
+        let mut idea = Idea {
+            data: bits,
+            bits_set: 0,
+            leading_zeros: 0,
+            trailing_zeros: 0
+        };
+
+        for bit in idea.data.iter() {
+            if bit {
+                idea.bits_set += 1;
+                idea.trailing_zeros = 0;
+            } else {
+                if idea.bits_set == 0 {
+                    idea.leading_zeros += 1;
+                }
+                idea.trailing_zeros += 1;
+            }
+        }
+
+        idea
+    }
+
+    fn into_bitvec(self) -> BitVec {
+        self.data
+    }
+}
+
+
+#[derive(Hash, Eq, PartialEq, Debug)]
+struct FragmentKey(Idea);
 
 /// `Fragment` represents several time slices
 /// of the spectrum within specified range.
@@ -20,9 +70,11 @@ struct Fragment {
     merge_factor: i32,
 }
 
+const FRAGMENT_SLICES: usize = 8;
+
 /// Internal container type used by `Dictionary`
 type RcFragment  = Rc<Fragment>;
-type FragmentMap = HashMap<FragmentKey, RcFragment>;
+type FragmentMap = Vec<(FragmentKey, RcFragment)>;
 
 /// `Dictionary` holds fragments associated with bit vectors.
 struct Dictionary<'a> {
@@ -32,7 +84,11 @@ struct Dictionary<'a> {
 
 impl FragmentKey {
     fn new() -> FragmentKey {
-        FragmentKey(BitVec::new())
+        FragmentKey(Idea::new())
+    }
+
+    fn from_bitvec(bits: BitVec) -> FragmentKey {
+        FragmentKey(Idea::from_bitvec(bits))
     }
 }
 
@@ -69,7 +125,7 @@ impl<'a> Dictionary<'a> {
     /// contains fragment that is similar enough to the one provided
     /// then fragments are merged together and the hash of result is
     /// returned. If no match was found, then new item is added.
-    fn insert(&mut self, fragment: Fragment) {
+    /*fn insert(&mut self, fragment: Fragment) {
         let mut new_key = fragment.key(self.detectors);
         let mut new_prototype = Rc::new(fragment);
 
@@ -98,13 +154,28 @@ impl<'a> Dictionary<'a> {
                 }
             }
         }
-    }
+    }*/
 
-    fn find(&self, key: &FragmentKey) -> Option<RcFragment> {
-        match self.map.get(key) {
-            Some(fragment) => Some(fragment.clone()),
-            None => None
+    fn find(&self, key: &FragmentKey, similarity: u32) -> Option<RcFragment> {
+        let mask = &key.0.bits();
+
+        for &(FragmentKey(ref candidate), ref value) in self.map.iter() {
+            assert_eq!(mask.len(), candidate.bits().len());
+
+            // Iterating through the keys looking for similarities.
+            // When there are more than `similarity` matched bits
+            // keys are said to be matching. Otherwise it is a no-match.
+            let mut total_match = 0;
+            for (b1, b2) in mask.blocks().zip(candidate.bits().blocks()) {
+                total_match += (b1 & b2).count_ones();
+
+                if total_match >= similarity {
+                    return Some(value.clone());
+                }
+            }
         }
+
+        None
     }
 
     fn merge(prototype: &mut Fragment, new_prototype: &Fragment) -> FragmentKey {
